ed6130f0c40525971678f0950209ede2ec5b880f
zhangmaosen <xiaosen.zhang@gmail.com>
2023-03-24 16:31:29 +0800

    mine new block

--- node.py
+++ node.py
@@ -11,9 +11,17 @@ connection_pool = ConnectionPool()  # <2>
 server = Server(blockchain, connection_pool, P2PProtocol)
 
 
-async def main():
+
+    
+async def serve():
     # Start the server
     await server.listen()
 
+async def mine():
+    await blockchain.mine_new_block()
 
+
+async def main():
+    asyncio.gather(serve(), mine())
+    
 asyncio.run(main())
